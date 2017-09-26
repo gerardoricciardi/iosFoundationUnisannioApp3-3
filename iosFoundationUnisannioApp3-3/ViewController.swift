c78a5c0dba83f3b730c596663248a0257a60df16
Ricciardi Gerardo <ricciardigerardo@san-06.local>
2017-09-26 17:19:33 +0200

    Initial Commit

---
+++ iosFoundationUnisannioApp3-3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iosFoundationUnisannioApp3-3
+//
+//  Created by Ricciardi Gerardo on 26/09/17.
+//  Copyright © 2017 Ricciardi Gerardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
