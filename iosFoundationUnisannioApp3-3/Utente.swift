2458abbfb9d125a0315606d40956adb9fda50750
Pagliaro Antonio <pagliaroantonio@san-17.local>
2017-09-27 13:09:29 +0200

    classe Utente init

--- iosFoundationUnisannioApp3-3/Utente.swift
+++ iosFoundationUnisannioApp3-3/Utente.swift
@@ -11,9 +11,9 @@ import Foundation
 class Utente{
     
     var livelloAttivita : Int!  // 0=principiante 1=intermedio 2=esperto
-    var sesso : Bool!       //true= female false= male
+    var sesso : Bool!      //true= female false= male
     var problemi: Int!  // 0= benessere 1=prob1 2=prob2 3=prob3
-    var nome,cognome,email,password : String
+    var nome,cognome,email,password : String!
         
     init(nome:String,cognome : String, email : String, password : String){
         self.nome=nome
@@ -21,7 +21,20 @@ class Utente{
             self.email=email
             self.password=password
         }
-        
+    
+    
+    init(nome:String,cognome : String, email : String, password : String,livelloAttivita : Int, sesso : Bool, problemi: Int){
+        self.nome=nome
+        self.cognome=cognome
+        self.email=email
+        self.password=password
+        self.livelloAttivita=livelloAttivita
+        self.sesso=sesso
+        self.problemi=problemi
+    }
+    
+    
+    
     }
     
 
