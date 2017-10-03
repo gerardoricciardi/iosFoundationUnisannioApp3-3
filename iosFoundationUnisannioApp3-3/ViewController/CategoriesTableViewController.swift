c560ef73898603433348bbb8ab72540f6af5c9dc
Ricciardi Gerardo <ricciardigerardo@san-06.local>
2017-10-03 12:08:56 +0200

    contactsTableview/ cell

---
+++ iosFoundationUnisannioApp3-3/ViewController/CategoriesTableViewController.swift
@@ -0,0 +1,115 @@
+//
+//  CategoriesTableViewController.swift
+//  iosFoundationUnisannioApp3-3
+//
+//  Created by Ricciardi Gerardo on 03/10/17.
+//  Copyright © 2017 Ricciardi Gerardo. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesTableViewController: UITableViewController {
+
+    let color = UIColor(red: 55.0/255.0, green: 153.0/255.0, blue: 178.0/255.0, alpha: 1.0)
+    var categoriesThumb = ["stretching", "pilates", "totalBody"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tableView.backgroundColor = color
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return categoriesThumb.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoriesCell", for: indexPath) as! CategoriesTableViewCell
+
+        // Configure the cell...
+        cell.categoriesImage.image = UIImage(named: categoriesThumb[indexPath.row])
+        cell.categoriesLabel.text = categoriesThumb[indexPath.row]
+        cell.backgroundColor = color
+
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetails"{
+            if let indexPath = tableView.indexPathForSelectedRow{
+                let destinationController = segue.destination as!DetailsViewController
+                // destinationController.restaurantName = immagini[indexPath.row]
+                //destinationController.locationName = location[indexPath.row]
+                //destinationController.typeName = type[indexPath.row]
+                destinationController.ristorante = ristoranti[indexPath.row]
+                
+                
+            }
+        }
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
