2461d11caa19e5760da6182edcbd00e335ad0db6
Fabio dell'Infante <fabiodellinfante@10-25-48-27.0200.san.it.iosda.org>
2017-10-04 10:28:42 +0200

    modifiche

--- iosFoundationUnisannioApp3-3/ViewController/ViewControllerProfilo.swift
+++ iosFoundationUnisannioApp3-3/ViewController/ViewControllerProfilo.swift
@@ -8,13 +8,28 @@
 
 import UIKit
 
-class ViewControllerProfilo: UIViewController {
-    
+class ViewControllerProfilo: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    let  picker = UIImagePickerController()
     @IBAction func CambiaImmagineProfilo(_ sender: UIButton) {
-        let alertController = UIAlertController(title: "Action Sheet", message: "What would you like to do?", preferredStyle: .actionSheet)
         
-        let sendButton = UIAlertAction(title: "Cambia Immagine", style: .default, handler: { (action) -> Void in
+        
+       let alertController = UIAlertController(title: "Action Sheet", message: "What would you like to do?", preferredStyle: .actionSheet)
+        
+        let ChangeImage = UIAlertAction(title: "Cambia Immagine", style: .default, handler: { (action) -> Void in
             print("Ok button tapped")
+            self.picker.allowsEditing = false
+            self.picker.sourceType = .photoLibrary
+            self.present(self.picker,animated: true,completion: nil)
+        })
+        
+        let  cameraButton = UIAlertAction(title: "Scatta una foto", style: .default, handler: { (action) -> Void in
+            print("Delete button tapped")
+            self.picker.allowsEditing = false
+            self.picker.sourceType = UIImagePickerControllerSourceType.camera
+            self.picker.cameraCaptureMode = .photo
+            self.picker.modalPresentationStyle = .fullScreen
+            self.present(self.picker,animated: true,completion: nil)
+            
         })
         
         let cancelButton = UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) -> Void in
@@ -22,7 +37,8 @@ class ViewControllerProfilo: UIViewController {
         })
         
         
-        alertController.addAction(sendButton)
+        alertController.addAction(ChangeImage)
+        alertController.addAction(cameraButton)
         alertController.addAction(cancelButton)
         
         self.navigationController!.present(alertController, animated: true, completion: nil)
@@ -33,7 +49,9 @@ class ViewControllerProfilo: UIViewController {
     @IBOutlet weak var immProfilo: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        immProfilo.layer.cornerRadius = 40.0
+        
+        self.picker.delegate = self
+        immProfilo.layer.cornerRadius = 30.0
         immProfilo.clipsToBounds = true
         // Do any additional setup after loading the view.
     }
@@ -53,5 +71,12 @@ class ViewControllerProfilo: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let immSel = info[UIImagePickerControllerOriginalImage] as? UIImage
+        {
+            immProfilo.contentMode = .scaleAspectFit
+            immProfilo.image = immSel
+        }
+        dismiss(animated: true, completion: nil)
+    }
 }
