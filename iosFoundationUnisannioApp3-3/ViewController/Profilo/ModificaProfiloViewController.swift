089e858794ff645e2bc0a28893e3b1af7be1c9d2
Fabio dell'Infante <fabiodellinfante@san-18.local>
2017-10-09 09:27:24 +0200

    barre di progresso

--- iosFoundationUnisannioApp3-3/ViewController/Profilo/ModificaProfiloViewController.swift
+++ iosFoundationUnisannioApp3-3/ViewController/Profilo/ModificaProfiloViewController.swift
@@ -10,51 +10,54 @@ import UIKit
 
 class ModificaProfiloViewController: UIViewController {
     
-    @IBOutlet weak var orarioFineP: UIDatePicker!
-    @IBOutlet weak var orarioInizioP: UIDatePicker!
-    let defaults=UserDefaults.standard
-    
-    @IBAction func WomanButton(_ sender: UIButton) {
-        defaults.set("donna", forKey: "Sesso")
-        defaults.synchronize()
-        print("donna")
-    }
-    @IBAction func ManButton(_ sender: UIButton) {
-        defaults.set("uomo", forKey: "Sesso")
-        defaults.synchronize()
-        print("uomo")
-    }
-    @IBAction func SaveButton(_ sender: UIBarButtonItem) {
-        defaults.set(orarioInizioP.date, forKey: "oraInizio")
-        defaults.set(orarioFineP.date, forKey: "oraFine")
-        defaults.synchronize()
-        print("Tutto registrato")
-        let popOverVC = UIStoryboard(name:"Utente", bundle: nil).instantiateViewController(withIdentifier: "sdPopUpID") as! PopUpViewController
-        self.addChildViewController(popOverVC)
-        popOverVC.view.frame = self.view.frame
-        self.view.addSubview(popOverVC.view)
-        popOverVC.didMove(toParentViewController: self)
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+//    @IBOutlet weak var orarioFineP : UIDatePicker!
+//    @IBOutlet weak var orarioInizioP : UIDatePicker!
+//    let defaults=UserDefaults.standard
+//
+//    @IBAction func WomanButton(_ sender: UIButton) {
+//        defaults.set("donna", forKey: "Sesso")
+//        defaults.synchronize()
+//        print("donna")
+//    }
+//    @IBAction func ManButton(_ sender: UIButton) {
+//        defaults.set("uomo", forKey: "Sesso")
+//        defaults.synchronize()
+//        print("uomo")
+//    }
+//    @IBAction func SaveButton(_ sender: UIBarButtonItem) {
+//        defaults.set(orarioInizioP.date, forKey: "oraInizio")
+//        defaults.set(orarioFineP.date, forKey: "oraFine")
+//        defaults.synchronize()
+//        print("Tutto registrato")
+//        let popOverVC = UIStoryboard(name:"Utente", bundle: nil).instantiateViewController(withIdentifier: "sdPopUpID") as! PopUpViewController
+//        self.addChildViewController(popOverVC)
+//        popOverVC.view.frame = self.view.frame
+//        self.view.addSubview(popOverVC.view)
+//        popOverVC.didMove(toParentViewController: self)
+//    }
+//
+//
+//
+//
+//    /*
+//    // MARK: - Navigation
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destinationViewController.
+//        // Pass the selected object to the new view controller.
+//    }
+//    */
+//
 }
+
